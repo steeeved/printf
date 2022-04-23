614cf292684b450bd512cfae8c5466ee1bbf5d22
Stephen Muchendu <muchendumaina66@gmail.com>
2022-04-23 14:15:20 +0300

    Kicks of a dying donkey

--- test/main.c
+++ test/main.c
@@ -1,58 +1,43 @@
-#include "main.h"
 #include <limits.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	int alen, elen;
-	char c = 'z';
-	char *str = NULL;
-	printf("START OF TEST\n");
-	printf("=====================\n");
-	printf("printf(NULL)  : %d\n", printf(str));
-	printf("_printf(NULL) : %d\n", _printf(str));
-	printf("printf(\"%\") : %d\n", printf("%"));
-	printf("_printf(\"%\"): %d\n", _printf("%"));
-	str = "hello, world";
-	printf("=====================\n");
-	printf("*****CHAR*****\n");
-	printf("=====================\n");
-	printf("Expected   : %c\n", 'a');
-	_printf("Actual     : %c\n", 'a');
-	elen = printf("Expected   : %c\n", c);
-	alen = _printf("Actual     : %c\n", c);
-	printf("Expected   : %i\n", elen);
-	printf("Actual     : %i\n", alen);
-	printf("Expected   : %cc\n", 'a');
-	_printf("Actual     : %cc\n", 'a');
-	printf("Expected   : %c\n", 53);
-	_printf("Actual     : %c\n", 53);
-	printf("Expected   : %c.\n", '\0');
-	_printf("Actual     : %c.\n", '\0');
-	printf("Expected   : %%%c\n", 'y');
-	_printf("Actual     : %%%c\n", 'y');
-	printf("=====================\n");
-	printf("*****STRINGS*****\n");
-	printf("=====================\n");
-	alen = elen = 0;
-	printf("Expected   : %s\n", "main");
-	_printf("Actual     : %s\n", "main");
-	printf("Expected   : %s$\n", "");
-	_printf("Actual     : %s$\n", "");
-	elen = printf("Expected   : %s\n", str);
-	alen = _printf("Actual     : %s\n", str);
-	elen = printf("%s", "");
-	alen = _printf("%s", "");
-	printf("Expected   : %i\n", elen);
-	printf("Actual     : %i\n", alen);
-	printf("Expected   : %sschool\n", "main");
-	_printf("Actual     : %sschool\n", "main");
-	elen = printf("Expected: %c%%%c%%%%%s%c%s%s\n", INT_MAX, INT_MIN, "hello", 0, "main", 0);
-	alen = _printf("Actual  : %c%%%c%%%%%s%c%s%s\n", INT_MAX, INT_MIN, "hello", 0, "main", 0);
-	printf("Expected: %d\n", elen);
-	printf("Actual  : %d\n", alen);
-	printf("Expected  %i %d\n", 0032, 0032);
-	_printf("Actual %i %d \n", 0032, 0032);
-	return (0);
-}
+    int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    ui = (unsigned int)INT_MAX + 1024;
+    addr = (void *)0x7ffe637541f0;
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+    _printf("Unsigned:[%u]\n", ui);
+    printf("Unsigned:[%u]\n", ui);
+    _printf("Unsigned octal:[%o]\n", ui);
+    printf("Unsigned octal:[%o]\n", ui);
+    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    _printf("Address:[%p]\n", addr);
+    printf("Address:[%p]\n", addr);
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+    _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
+    return (0);
